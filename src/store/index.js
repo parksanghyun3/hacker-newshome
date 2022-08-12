753b9513157a2051d543f69109c9e3adf53d4cc0
PARKSANGHYUN3 <basira@easymedia.net>
2022-08-12 17:55:36 +0900

    ask mapState까지 완료

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { fetchNewsList } from "../api/index.js"
+import { fetchNewsList, fetchJobsList, fetchAskList } from "../api/index.js"
+// import { fetchJobsList } from "../api/index.js"
+// import { fetchAskList } from "../api/index.js"
 
 Vue.use(Vuex);
 
@@ -10,14 +12,23 @@ export const store = new Vuex.Store({
     //프로젝트에서 공통으로 사용할 변수를 정의하는 곳
     //프로젝트 내의 모든 곳에서 참조 및 사용이 가능합니다.
     news: [],
+    jobs: [],
+    ask: [],
   },
   mutations: {
     // state를 변경시키는 역할 (mutations를 통해서만 state를 변경할 수 있다.)
     // 비동기처리가 아닌 동기처리가 일어나는 구간
     // mutations 내에 함수 형태로 작성해야함
-    SET_NEWS(state, news){
-      state.news = news;
-    }
+    // 기본 인자는 payload로 받아야 한다.
+    SET_NEWS(state, payload){
+      state.payload = payload;
+    },
+    SET_JOBS(state, payload){
+      state.payload = payload;
+    },
+    SET_ASK(state, payload){
+      state.payload = payload;
+    },
   },
   actions: {
     // mutations을 실행시키는 역할
@@ -28,6 +39,20 @@ export const store = new Vuex.Store({
       fetchNewsList()
       .then(response => context.commit("SET_NEWS", response.data)) 
       .catch(error => console.log(error))
+    },
+    // distructuring
+    FETCH_JOBS({ commit }){
+      fetchJobsList()
+      .then(({ data }) => commit("SET_JOBS", data)) 
+      .catch(error => console.log(error))
+    },
+
+    // distructuring
+    FETCH_ASK({ commit }){
+      fetchAskList()
+      .then(({ data }) => commit("SET_ASK", data))
+      .catch(error => console.log(error));
+
     }
   }
 });
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -6,24 +6,35 @@
 
 <script>
 // import console from 'console';
-import { fetchAskList } from '../api/index.js';
+// import { fetchAskList } from '../api/index.js';
  
+import { mapState } from 'vuex'
 
 export default {
-  data(){
-    return {
-      ask: []
-    }
-  },  
+  // data(){
+  //   return {
+  //     ask: []
+  //   }
+  computed: {
+    ...mapState({
+        // ask 를 컴퓨티드로 만들었기 때문에 위에 v-for 문에 사용가능 this.$store.state.payload -> ask
+        ask: state => state.payload
+      })
+    // ask(){
+
+      // return this.$store.state;
+    // }
+  },
   created(){
-    fetchAskList()
-      .then(response => this.ask = response.data)
-      .catch(error => console.log(error));
-    
-  }
+    this.$store.dispatch("FETCH_ASK");
+  //   fetchAskList()
+  //     .then(response => this.ask = response.data)
+  //     .catch(error => console.log(error));
+  // }
+  },  
 }
 </script>
 
 <style>
 
-</style>
+</style> 
